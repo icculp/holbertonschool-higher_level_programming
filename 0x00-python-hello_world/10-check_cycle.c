9bf2e2957bed6bf907cbc0c420aef163c967309b
Ian Culp <ian.culp@holbertonschool.com>
2020-04-27 21:46:22 +0000

    Adding 10, unfinished

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+* check_cycle - Checks if a linked list has a cycle in it
+* @list: Pointer to linked list
+* Return: Success or fail
+*/
+
+int check_cycle(listint_t *list)
+{
+	int i = 0;
+	listint_t *temp = NULL;
+
+	printf("%d\n", i);
+	return (0);
+}
