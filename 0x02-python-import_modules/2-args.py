ab16a773a53be0c3f604969486edf37c9ae28b6b
Ian Culp <icculp@gmail.com>
2020-04-30 17:57:26 +0000

    Edge of 1 arg on 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,10 @@ if __name__ == "__main__":
     import sys
     if (len(sys.argv) == 1):
         print("0 arguments.")
+    elif (len(sys.argv) == 2):
+        print("{} argument:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
