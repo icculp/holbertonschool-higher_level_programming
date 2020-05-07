fe135a6a8d7535bbc19cc2fdbd4e6f7d362070e9
Ian Culp <icculp@gmail.com>
2020-05-07 17:49:37 +0000

    Trying 2 with different method for empty lists

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    sum = 0
     if (len(my_list) == 0):
-        return None
+        return sum
     new1 = my_list.copy()
     new1.sort()
-    sum = 0
     for i in range(len(new1)):
         if new1[i] == new1[i - 1]:
             continue
