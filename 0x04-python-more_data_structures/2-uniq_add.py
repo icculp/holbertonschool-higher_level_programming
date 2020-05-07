f68150aea39b90e1f0d31b9f15610fa598f8e607
Ian Culp <icculp@gmail.com>
2020-05-07 17:55:14 +0000

    Accounting for 1 item list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,8 @@ def uniq_add(my_list=[]):
     sum = 0
     if (len(my_list) == 0):
         return sum
+    if (len(my_list) == 1):
+        return my_list[0]
     new1 = my_list.copy()
     new1.sort()
     for i in range(len(new1)):
