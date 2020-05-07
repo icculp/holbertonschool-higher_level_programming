1da5b79df5311bbc3bef7b242d0fe7a71ba982c5
Ian Culp <icculp@gmail.com>
2020-05-07 18:09:53 +0000

    Last edge for 2, long list all same values

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,11 +3,12 @@ def uniq_add(my_list=[]):
     sum = 0
     if (len(my_list) == 0):
         return sum
+    sum = my_list[0]
     if (len(my_list) == 1):
-        return my_list[0]
+        return sum
     new1 = my_list.copy()
     new1.sort()
-    for i in range(len(new1)):
+    for i in range(1, len(new1)):
         if new1[i] == new1[i - 1]:
             continue
         else:
