f993541368e63ecbb995b2a0808ccc016e7aef1c
Ian Culp <icculp@gmail.com>
2020-05-07 18:12:56 +0000

    Sort before accessing index 0

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,11 +3,11 @@ def uniq_add(my_list=[]):
     sum = 0
     if (len(my_list) == 0):
         return sum
-    sum = my_list[0]
-    if (len(my_list) == 1):
-        return sum
     new1 = my_list.copy()
     new1.sort()
+    sum = new1[0]
+    if (len(my_list) == 1):
+        return sum
     for i in range(1, len(new1)):
         if new1[i] == new1[i - 1]:
             continue
