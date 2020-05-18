50a6fe05ec250aa82ce9fe2b4556e6475b143530
Ian Culp <icculp@gmail.com>
2020-05-18 17:39:11 +0000

    Simplified 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
     def raise_exception_msg(message=""):
-        class NameError(Exception):
-            def __init__(self, message):
-                self.message = message
-        raise NameError(message)
+        try:
+            raise NameError(message)
+        except NameError:
+            print(message)
