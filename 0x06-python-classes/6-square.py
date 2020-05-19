15acd35fded4b3ae43a31d2bcdfdc7086142040c
Ian Culp <icculp@gmail.com>
2020-05-19 21:32:00 +0000

    Gosh darn docstrings

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,15 +6,18 @@ class Square:
     """Defines a square"""
 
     def __init__(self, size=0, position=(0, 0)):
+        """INIT"""
         self.__size = size
         self.__position = position
 
     @property
     def size(self):
+        """FUCKYOU"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """FUCKYOU"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -24,20 +27,24 @@ class Square:
 
     @property
     def position(self):
+        """STUPID"""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple)
+        """GOSHDANGDOCSTRING"""
+        if not isinstance(value, tuple)\
         or __position[0] < 0 or __position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
     def area(self):
+        """RETURNS AREA"""
         return (self.__size*self.__size)
 
     def my_print(self):
+        """Prints a square"""
         if self.__size == 0:
             for x in range(self.__position[1]):
                 print()
