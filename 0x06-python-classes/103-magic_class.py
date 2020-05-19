de06304702326ecddad907dba00cc85e2ecf279d
Ian Culp <icculp@gmail.com>
2020-05-19 23:50:55 +0000

    More docstrings, fml

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,12 +4,12 @@ import math
 
 
 class MagicClass:
-    """Defines a magicb"""
-
-    def __init__(self, radius=0):
+    '''Defines a magicb'''
+    def __init__(self, radius=3):
+        '''fuck you'''
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self, radius):
@@ -18,4 +18,5 @@ class MagicClass:
 
     def circumference(self, radius):
         '''asdfjl'''
+
         return 2 * math.pi * self.__radius
