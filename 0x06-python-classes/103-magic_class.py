113d178e450926519924d7727a866f512dd840a9
Ian Culp <icculp@gmail.com>
2020-05-19 23:37:45 +0000

    pep8, adjust import, adjust docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """D"""
+import math
 
 
 class MagicClass:
     """Defines a magicb"""
 
-    import math
-
     def __init__(self, radius=0):
-        """Initializing"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        self.__radius = radius
 
     def area(self, radius):
-        """Doing math and stuff"""
+        '''asdf'''
         return (self.__radius ** 2) * math.pi
 
     def circumference(self, radius):
-        """More maths"""
+        '''asdfjl'''
         return 2 * math.pi * self.__radius
