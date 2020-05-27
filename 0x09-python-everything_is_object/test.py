4b13c6a0fc8bd4d5b0e9efb5ec4534a69121ebe4
Ian Culp <icculp@gmail.com>
2020-05-27 04:18:49 +0000

    28

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+l1 = [1, 2, 3]
+l2 = l1
+l2[1] = 98
+l1[0] = 89
+print(l1)
+print(l2)
+l1 = l1 + [4]
+l2[1] = 100
+l1[0] = 111
+print(l1)
+print(l2)
+
+'''
 a = 5
 print(id(a))
 a = 6
@@ -19,4 +32,4 @@ del a
 print(id(b))
 del b
 c = "HBTN"
-print(id(c))
+print(id(c))'''
