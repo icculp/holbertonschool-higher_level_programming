2cd8d9a680052eb59ddbc1cc4dc4bb25221e9d27
Ian Culp <icculp@gmail.com>
2020-06-04 01:58:32 +0000

    Adding 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Task 100 Module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ appends after certain string """
+    with open(filename, 'r', encoding='UTF-8') as f:
+        count = 0
+        rl = f.readlines()
+        for i in range(len(rl)):
+            if search_string in rl[i]:
+                rl.insert(i + 1, new_string)
+                count += 1
+                continue
+        o = open(filename, 'w', encoding='utf-8')
+        o.writelines(rl)
