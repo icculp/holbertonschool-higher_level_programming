a9a2ceb272e725beff8935069d6a904d485ffcef
Ian Culp <icculp@gmail.com>
2020-06-04 00:53:47 +0000

    Adding 14, cleaning repo

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,12 +3,17 @@
     Task 14 module
 """
 
+
 def pascal_triangle(n):
     """ prints a neat triangle """
-    if n <= 0:
-        return list()
     tri = []
-    for i in range(n):
-        for k in range(i):
-            tri += [i + k]
+    if n <= 0:
+        return tri
+    for i in range(1, n + 1):
+        e = 1
+        row = []
+        for k in range(1, i + 1):
+            row.append(e)
+            e = int(e * (i - k) / k)
+        tri.append(row)
     return tri
