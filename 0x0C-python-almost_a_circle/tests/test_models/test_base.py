8ffbc1593309e7b9c15e42b025796dd83f1f1256
Ian Culp <icculp@gmail.com>
2020-06-09 17:27:11 +0000

    Cleaning repo

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,10 +13,12 @@ class TestBase(unittest.TestCase):
         self.b1 = Base(1)
         self.b2 = Base(2)
         self.base2 = Base()
+        self.base3 = Base("wrong")
+        self.basen = Base(-10)
 
     def test_base_class(self):
         """ test base class """
-        self.assertTrue(type(self.base) is Base)
+        self.assertIsInstance(self.base, Base)
         self.assertTrue(type(self.base2) is Base)
         self.assertTrue(type(self.b1) is Base)
         self.assertTrue(type(self.b2) is Base)
@@ -27,7 +29,18 @@ class TestBase(unittest.TestCase):
         self.assertEqual(self.b1.id, 1)
         self.assertEqual(self.b2.id, 2)
         self.assertEqual(self.base2.id, 2)
+        self.assertEqual(self.basen.id, -10)
+        self.assertEqual(self.base3.id, "wrong")
 
+    def test_base_wrong_2(self):
+        """ bad init """
+        self.assertRaises(TypeError, Base(), "wrong")
+
+    def test_to_json_string(self):
+        """ tests static method """
+        self.assertEqual(self.base.to_json_string([]), '[]')
+        self.assertEqual(self.base.to_json_string([{'x': 2}, {'width': 10}]), '[{\"x\": 2}, {\"width\": 10}]')
+        self.assertRaises(ValueError, self.base.to_json_string(), [{'1': 2}])
 
     def tearDown(self):
         """ done testing, tear down """
