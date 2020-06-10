4c32d7596ea95240f5b406d9ab2a3724654e9b55
Ian Culp <icculp@gmail.com>
2020-06-10 01:27:20 +0000

    Adding testssss

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Unittest module for testing models """
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Tests for Base class """
+
+    def setUp(self):
+        """ setup for tests """
+        self.rect = Rectangle(98, 98, id=98)
+        self.r1 = Rectangle(1, 1, 1, 1, 1)
+        self.r2 = Rectangle(2, 2, 2, 2, 2)
+
+    def test_rectangle_docstring(self):
+        """ tests for docstrings """
+        self.assertIsNotNone(("models.rectangle").__doc__)
+        self.assertIsNotNone(Rectangle.__doc__)
+
+    def test_rectangle_class(self):
+        """ test base class """
+        self.assertIsInstance(self.rect, Rectangle)
+        self.assertTrue(type(self.r1) is Rectangle)
+        self.assertTrue(type(self.r2) is Rectangle)
+        
+    def test_aase_id(self):
+        """ test base id """
+        self.assertEqual(self.rect.id, 98)
+        self.assertEqual(self.r1.id, 1)
+        self.assertEqual(self.r2.id, 2)
+
+    def tearDown(self):
+        """ done testing, tear down """
+        pass
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Unittest module for testing models """
+import unittest
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """ Tests for Base class """
