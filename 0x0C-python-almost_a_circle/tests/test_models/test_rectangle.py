04c76e66d3116696eaff58a9d81f3fa9ae316f0c
Ian Culp <icculp@gmail.com>
2020-06-10 02:10:37 +0000

    Mo tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -50,6 +50,27 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r1.area(), 1)
         self.assertEqual(self.r2.area(), 4)
 
+    def test_rectangle_str(self):
+        """ tests string up in this bitch """
+        rrr = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(str(rrr), "[Rectangle] (12) 2/1 - 4/6")
+
+    def test_rectangle_update(self):
+        """ tests update function """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/5 - 2/3")
+
+    def test_rectangle_update_more(self):
+        """ tests update function """
+        r1 = Rectangle(10, 10, 10, 10, 10)
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(str(r1), "[Rectangle] (10) 1/3 - 4/2")
+
+    def test_rectangle_to_dict(self):
+        """ tests to_dict function """
+        self.assertDictEqual(self.r2.to_dictionary(), {'id': 2, 'width': 2, 'height': 2, 'x': 2, 'y': 2})
+
     def tearDown(self):
         """ done testing, tear down """
         pass
