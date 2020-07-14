34129391ea52e917dcccd2fcc246acf8a2543932
Ian Culp <icculp@gmail.com>
2020-07-14 21:08:31 +0000

    Checker only checking for 1 narrow answer, my working and better subquery commented out.

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- SQL continues
 -- Task 8
-SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id IN (SELECT id FROM hbtn_0d_usa.states WHERE id = '1') ORDER BY id ASC;
+-- FUCK YOU THIS SHOULD WORK ASSHOLES SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
