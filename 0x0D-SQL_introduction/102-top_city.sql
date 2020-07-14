6f598a364d6553aeeff44d3543f39c68e6394e86
Ian Culp <icculp@gmail.com>
2020-07-14 04:46:17 +0000

    Wrong months

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- mysql project
 -- task 18/101
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (6, 7) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
