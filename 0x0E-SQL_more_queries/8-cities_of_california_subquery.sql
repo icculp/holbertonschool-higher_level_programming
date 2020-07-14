44813276c597f7659f1424f3ef7dd43080307b6c
Ian Culp <icculp@gmail.com>
2020-07-14 20:55:06 +0000

    state_id as id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- SQL continues
 -- Task 8
-SELECT id, name FROM hbtn_0d_usa.cities WHERE id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
+SELECT state_id AS id, name FROM hbtn_0d_usa.cities WHERE id IN (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California') ORDER BY id ASC;
