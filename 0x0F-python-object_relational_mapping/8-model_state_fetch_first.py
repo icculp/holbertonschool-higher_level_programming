42d45fc786f8a17ca52746e75b616810f8ec6445
Ian Culp <icculp@gmail.com>
2020-08-08 19:05:29 +0000

    using first()

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    for state in session.query(State).filter_by(id=1):
-        if state:
-            print("{}: {}".format(state.id, state.name))
-        else:
-            print("Nothing")
+    state = session.query(State).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
