4eddb4e8036cb00525e489d9a090913f7722bdf2
Ian Culp <icculp@gmail.com>
2020-08-08 18:50:43 +0000

    comment inside guard

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,14 +24,14 @@ if __name__ == '__main__':
     for state in session.query(State).filter_by(id=1).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
-'''
-    m = "mysql://{}:{}@{}:3306/{}".format(MY_USER, MY_PASS, MY_HOST, MY_DB)
-    engine = create_engine(m, encoding='latin1')
-    conn = engine.connect()
-    meta = MetaData(engine)
-    table = Table('states', meta, autoload=True, autoload_with=engine)
-    slct = select([table]).where(table.c.id == 1)
-    res = conn.execute(slct)
-    for row in res:
-        print(row)
-'''
+    '''
+       m = "mysql://{}:{}@{}:3306/{}".format(MY_USER, MY_PASS, MY_HOST, MY_DB)
+       engine = create_engine(m, encoding='latin1')
+       conn = engine.connect()
+       meta = MetaData(engine)
+       table = Table('states', meta, autoload=True, autoload_with=engine)
+       slct = select([table]).where(table.c.id == 1)
+       res = conn.execute(slct)
+       for row in res:
+           print(row)
+    '''
