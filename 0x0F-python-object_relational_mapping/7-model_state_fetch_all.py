4115d860d78698a7c2472271fe28de5ad2f73d55
Ian Culp <icculp@gmail.com>
2020-08-08 03:42:43 +0000

    7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+    Task 7
+'''
+from model_state import Base, State
+from sqlalchemy import create_engine, select, Table, MetaData
+import sys
+
+
+if __name__ == '__main__':
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    m = "mysql://{}:{}@{}:3306/{}".format(MY_USER, MY_PASS, MY_HOST, MY_DB)
+    engine = create_engine(m, encoding='latin1')
+    conn = engine.connect()
+    meta = MetaData(engine)
+    table = Table('states', meta, autoload=True, autoload_with=engine)
+    slct = select([table])
+    res = conn.execute(slct)
+    for row in res:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+    Task 6
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    ''' State sqlachlemy '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
