2c859a221f68fd65685044f5629927cbfdb75187
Ian Culp <icculp@gmail.com>
2020-08-08 00:42:02 +0000

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+    Task 5
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE = sys.argv[4]
+
+    conn = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER,
+                           passwd=MY_PASS, db=MY_DB, charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') \
+                FROM cities \
+                JOIN states \
+                ON cities.state_id=states.id \
+                WHERE cities.state_id=states.id \
+                AND states.name=%s \
+                ORDER BY cities.id ASC", (STATE,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row[0])
+    cur.close()
+    conn.close()
