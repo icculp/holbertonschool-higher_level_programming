da995fc211d4f41084ef82aca58823d9db8fa765
Ian Culp <icculp@gmail.com>
2020-08-08 17:53:06 +0000

    1 filter in print not query (bullshit)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER,
                            passwd=MY_PASS, db=MY_DB, charset='utf8')
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row:
+        if row[1][0] == 'N':
             print(row)
     cur.close()
     conn.close()
