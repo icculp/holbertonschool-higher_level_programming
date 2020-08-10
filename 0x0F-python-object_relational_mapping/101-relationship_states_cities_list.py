d7f11bc2df26662a29c9ba48d16f35e38e7f9f36
Ian Culp <icculp@gmail.com>
2020-08-10 04:14:53 +0000

    101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+    Task 101
+'''
+from relationship_state import Base, State
+from sqlalchemy import create_engine, select, Table, MetaData
+import sys
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+
+
+if __name__ == '__main__':
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    m = "mysql://{}:{}@{}:3306/{}".format(MY_USER, MY_PASS, MY_HOST, MY_DB)
+    engine = create_engine(m, encoding='latin1')
+    conn = engine.connect()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    sess = session.query(State)
+    for st in sess:
+        print("{}: {}".format(st.id, st.name))
+        for cty in st.cities:
+            print("\t{}: {}".format(cty.id, cty.name))
+    session.close()
