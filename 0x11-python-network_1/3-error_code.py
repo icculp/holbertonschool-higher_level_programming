39e04f5ad1805e2c6724b6898e9b2152b1af3581
Ian Culp <icculp@gmail.com>
2020-08-24 18:53:34 +0000

    Adding Error code:

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
         with request.urlopen(ur) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print(e.code)
+        print("Error code: {}".format(e.code))
