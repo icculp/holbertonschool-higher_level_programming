6ee73ea3a73bc4a664c0a9e077387fd5745529ed
Ian Culp <icculp@gmail.com>
2020-08-24 18:51:47 +0000

    3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Task 3
+"""
+from urllib import request
+from urllib import parse
+from urllib import error
+import sys
+
+
+if __name__ == '__main__':
+    ur = sys.argv[1]
+    try:
+        with request.urlopen(ur) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print(e.code)
