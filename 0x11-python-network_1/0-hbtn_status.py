06d5620aedf1b438eb878e6683c1e5a84f955f38
Ian Culp <icculp@gmail.com>
2020-08-24 17:38:52 +0000

    Adding 0x11

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Task 0
+"""
+from urllib import request
+
+
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    print("Body response:")
+    for thing in response:
+        print("\t - type: {}".format(type(thing)))
+        print("\t - content: {}".format(thing))
+        print("\t - utf8 content: {}".format(thing.decode("utf-8")))
