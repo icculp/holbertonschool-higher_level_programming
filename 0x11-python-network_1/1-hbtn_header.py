6a04bc9c6b9fc4d370fb57f244a3756134cae6ac
Ian Culp <icculp@gmail.com>
2020-08-24 17:57:09 +0000

    Removing comments

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,9 +9,3 @@ from sys import argv
 ur = argv[1]
 with request.urlopen(ur) as response:
     print(response.getheader('X-Request-Id'))
-'''
-    for thing in response:
-        print("\t- type: {}".format(type(thing)))
-        print("\t- content: {}".format(thing))
-        print("\t- utf8 content: {}".format(thing.decode("utf-8")))
-'''
