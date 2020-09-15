25fa19937bcfcfba141f28969b72f7a484d4b888
Ian Culp <icculp@gmail.com>
2020-09-15 18:47:50 +0000

    6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing\n';
+const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
+let s = '';
 let i;
 
 for (i = 0; i < myVar.length; i++) {
-  process.stdout.write(myVar[i]);
+  s += myVar[i];
 }
+console.log(s);
