9b6f79f88a6fd4249c55d3d834b6605e67bde049
Ian Culp <icculp@gmail.com>
2020-09-21 17:18:42 +0000

    2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+* Task 2
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+    }
+  }
+}
+module.exports = Rectangle;
