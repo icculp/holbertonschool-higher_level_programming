2db149bec9846380b5b5cb565fbcafd25b018105
Ian Culp <icculp@gmail.com>
2020-09-21 21:59:44 +0000

    13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+* Task 13
+*/
+const fs = require('fs');
+const a = process.argv[2];
+const b = process.argv[3];
+const c = process.argv[4];
+fs.readFile(a, 'utf-8', function (err, data) {
+  if (err) throw err;
+  fs.writeFile(c, data, function (err) { if (err) throw err; });
+});
+fs.readFile(b, 'utf-8', function (err, data) {
+  if (err) throw err;
+  fs.appendFile(c, data, function (err) { if (err) throw err; });
+});
