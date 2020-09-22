5f37b5fcb5ea1ce9c0ec04bf6e162bb5f0b20569
Ian Culp <icculp@gmail.com>
2020-09-22 22:47:35 +0000

    7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -10,8 +10,16 @@ req(url, function (error, response, body) {
     console.log(error);
   } else {
     const jj = JSON.parse(body);
-    for (let i = 0; i < jj['characters'].length; i++) {
-      console.log(jj.characters[i]);
+    for (let i = 0; i < jj.characters.length; i++) {
+      const newurl = jj.characters[i];
+      req(newurl, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          const j = JSON.parse(body);
+          console.log(j.name);
+        }
+      });
     }
   }
 });
