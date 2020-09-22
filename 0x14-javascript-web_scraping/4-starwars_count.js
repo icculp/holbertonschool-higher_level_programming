b9f03577243e85b7f0689daba5c2b9eb3a75d7bb
Ian Culp <icculp@gmail.com>
2020-09-22 20:57:42 +0000

    4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+* Task 4
+*/
+const req = require('request');
+const url = process.argv[2];
+req(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const jj = JSON.parse(body);
+    let count = 0;
+    for (let i = 0; i < jj.results.length; i++) {
+      for (let j = 0; j < jj.results[i].characters.length; j++) {
+        if (jj.results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
