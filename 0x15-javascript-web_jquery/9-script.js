f3a372a5a14e27df0fd61f75d381b5703baf74a9
Ian Culp <icculp@gmail.com>
2020-09-24 22:21:32 +0000

    Left console.log

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 /*
-* Task 8
+* Task 9
 */
 const $ = window.$;
 $.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  console.log(data.results);
   for (const r of data.results) {
     $('ul#list_movies').append('<li>' + r.title + '</li>\n </br>');
   }
